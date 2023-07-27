a42df311c5c997fe15ea8858df8dd32823b80b76
mgrodriguez1 <mgrodriguez@stefanini.com>
2023-07-27 13:14:43 -0500

    Update comments

--- src/main/java/com/example/demo/api/ClientesApi.java
+++ src/main/java/com/example/demo/api/ClientesApi.java
@@ -7,20 +7,15 @@ import java.util.Map;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
-
 import com.fasterxml.jackson.core.exc.StreamReadException;
 import com.fasterxml.jackson.databind.DatabindException;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -31,8 +26,8 @@ import java.io.IOException;
 public class ClientesApi extends ResponseEntityExceptionHandler{
 
     
+	@SuppressWarnings("deprecation")
 	@RequestMapping(value="/cliente", method=RequestMethod.GET)
-	//@ExceptionHandler(Exception.class)
 	@ExceptionHandler(HttpClientErrorException.class)
     public String getById(@RequestParam char tipoDocumento, @RequestParam String numeroDocumento) throws StreamReadException, DatabindException, IOException{
 		String respuesta = "Ok";
@@ -42,16 +37,17 @@ public class ClientesApi extends ResponseEntityExceptionHandler{
 
         // Read the JSON file.
         ObjectMapper mapper = new ObjectMapper();
-        Map<String, Object> map = mapper.readValue(new File("db.json"), Map.class);
+        @SuppressWarnings("unchecked")
+		Map<String, Object> map = mapper.readValue(new File("db.json"), Map.class);
         map.remove("posts");
-        // Add a new employee to the map.
+        // Add a new client to the map.
         Map<String, String> clte = new HashMap<>();
         clte.put("primerNombre", "Pedro");
         clte.put("segundoNombre", "Pablo");
         clte.put("primerApellido", "Perez");
         clte.put("segundoApellido", "Perdomo");
         clte.put("telefono", "+57 311 222 3344");
-        clte.put("direccion", "Suba");
+        clte.put("direccion", "Cra 7 # 7 - 70");
         clte.put("ciudadResidencia", "Bogota");
         map.put("posts", Arrays.asList(map.get("posts"), clte));
         // Write the map to a JSON file.
